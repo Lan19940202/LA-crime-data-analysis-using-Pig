a94a9dcc9ee687ed8af7bac24c25edae44695fcf
Lan Wei <35413852+Lan19940202@users.noreply.github.com>
2018-02-12 15:32:38 -0600

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,8 +1,8 @@
 sqoop import \
 --connect jdbc:mysql://fordbuan6346.caog2tqbzzhd.us-west-2.rds.amazonaws.com/buan6346 \
 --driver com.mysql.jdbc.Driver \
---username buan6346 \
---password buan6346 \
+--username xxxx \
+--password xxxx \
 -m 1 \
 --table la_crime \
 --columns "DR_NUMBER,DATE_REPORTED,DATE_OCCURED,TIME_OCCURED,AREA_ID,AREA_NAME,REPORTING_DISTRICT,CRIME_CODE,CRIME_CODE_DESC,VICTIM_AGE,VICTIM_GENDER,VICTIM_DESCENT,ADDRESS,CROSS_STREET,AUTO_ID" \
